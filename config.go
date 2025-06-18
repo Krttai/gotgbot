ce374ea981c9bc101f300ab436e08d62e47a5a65
Krttai <162029519+Krttai@users.noreply.github.com>
2025-06-18 08:41:16 +0530

    Create config.go

---
+++ config.go
@@ -0,0 +1,18 @@
+package gotgbot
+
+var (
+	API_ID             = 20196547
+	API_HASH           = "f1d32b4f43d2e1d50b863b1a77ddf2a2"
+	BOT_TOKEN          = "5492813408:AAEd0YAOV4ITTocCFxt_kSBJZeW7cwanFQs"
+	ADMINS             = []int64{1315244338}
+	DATABASE_NAME      = "Cluster0"
+	MONGODB_URI        = "mongodb+srv://h62857505:T%239gLp%403Vz%21Xq7Wd@cluster0.arh3lft.mongodb.net/"
+	LOG_CHANNEL        = int64(-1001992530532)
+	AUTH_CHANNEL       = int64(-1002768484256)
+	FILE_STORE_CHANNEL = int64(-1002287265387)
+	CHANNELS           = []int64{
+		-1002882858378,
+		-1002736316980,
+		-1002765341804,
+	}
+)
